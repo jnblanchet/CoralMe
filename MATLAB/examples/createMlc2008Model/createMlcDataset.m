5846cd801a1fde352b3f63b8a7d08501d85af733
jnblanchet <jean.nicola.blanchet@gmail.com>
2016-05-18 19:59:55 -0400

    make ds

--- MATLAB/examples/createMlc2008Model/createMlcDataset.m
+++ MATLAB/examples/createMlc2008Model/createMlcDataset.m
@@ -1,31 +1,34 @@
 ds = Dataset();
 dsTs = Dataset();
 
-ds.representations = {'TextonExtractor'} %, 'ClbpExtractor'};
+ds.representations = {'TextonExtractor'};
 dsTs.representations = ds.representations;
 
-trMap = true(1,size(data,1),'logical');
-trMap(2:10:size(data,1)) = false;
+% trMap = true(1,size(data,1),'logical');
+% trMap(1:10:size(data,1)) = false;
 
 for i=numel(ds.representations):-1:1
     if i == 3
-        ds.featureMatrices{i} = cell2mat(data(trMap,i)')';
-        dsTs.featureMatrices{i} = cell2mat(data(~trMap,i)')';
+        ds.featureMatrices{i} = cell2mat(data(:,i)')';
+%         dsTs.featureMatrices{i} = cell2mat(data(~trMap,i)')';
     else
-        ds.featureMatrices{i} = cell2mat(data(trMap,i));
-        dsTs.featureMatrices{i} = cell2mat(data(~trMap,i));
+        ds.featureMatrices{i} = cell2mat(data(:,i));
+%         dsTs.featureMatrices{i} = cell2mat(data(~trMap,i));
     end
 end
 
 ds.labelDescriptions = labelNames;
-ds.labels = labels(trMap);
+ds.labels = labels(:);
 
 ds.rebuildModel();
 
-ds.predictClasses(dsTs);
+% ds.predictClasses(dsTs);
 
-labels(trMap)
+ds.save('mlc2008_200PerClass_textons')
 
-sum(dsTs.labels == labels(~trMap)') / numel(dsTs.labels == labels(~trMap)')
+% [conf,rate]=confmat(lbl,labels(~trMap));
+% imagesc(conf)
+% colorbar()
+% title(num2str(rate))
 
 % ds.save('mlc2008_200PerClass_fusion2')
