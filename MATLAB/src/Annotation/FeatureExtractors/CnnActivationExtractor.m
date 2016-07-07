185f5d8919f5df4f1923fb21331ae763fc6164b3
jnblanchet <jean.nicola.blanchet@gmail.com>
2016-07-07 11:23:05 -0400

    CNN patch extraction was incorrect

--- MATLAB/src/Annotation/FeatureExtractors/CnnActivationExtractor.m
+++ MATLAB/src/Annotation/FeatureExtractors/CnnActivationExtractor.m
@@ -10,11 +10,10 @@ classdef CnnActivationExtractor < AbstractTextureRepresentationExtractor
         function features = extractFeatureFunction(this, x0, x1, y0, y1, mask, image, imageName)
             % crop the mask
             bb = regionprops(mask,'BoundingBox');
-            coords = cell2mat(struct2cell(bb)');
-            coords(:,3:4) = coords(:,3:4) + coords(:,1:2);
-            box = round([min(coords(:,1:2),[],1),max(coords(:,3:4),[],1)-.5]);
+            coords = round(cell2mat(struct2cell(bb)'));
+            box = [coords(2), coords(2) + coords(4), coords(1), coords(1) + coords(3)];
             
-            features = extractCnnActivation(box(1), box(3), box(2), box(4), image);
+            features = extractCnnActivation(box(1), box(2), box(3), box(4), image);
         end
     end
     
